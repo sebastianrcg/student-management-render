1faa57e09eb52791f31c885c8c12a548c1095cc7
Sebastian Cruz <sebalox92@gmail.com>
2026-02-13 21:16:25 -0400

    Copy build folder outside client, change express.static('build')

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const morgan = require('morgan');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "public")));
+//app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static('build'));
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"))
